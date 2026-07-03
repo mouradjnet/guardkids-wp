1fda56cbab6bcbb4bab71b650a9667a55747e04b
mouradjnet <djairfalcaoonline@gmail.com>
2026-07-03 13:18:31 -0300

    feat(medals): migração 020 medal_unlocks + bump DB v20 + uninstall

--- uninstall.php
+++ uninstall.php
@@ -34,6 +34,7 @@ $tables = [
     $wpdb->prefix . 'guardkids_progression',
     $wpdb->prefix . 'guardkids_progression_awards',
     $wpdb->prefix . 'guardkids_mission_completions',
+    $wpdb->prefix . 'guardkids_medal_unlocks',
 ];
 
 foreach ($tables as $table) {
